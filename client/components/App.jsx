ae13bf08ef7ec68061e81323f0d70f3cbcea10b0
randy-r <31799713+randy-r@users.noreply.github.com>
2017-09-09 23:40:03 +0300

    ESLint.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
-export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
-  }
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hello World</h1>
+    </div>);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
   },
-  plugins: [HtmlWebpackPluginConfig]
-}
+  plugins: [HtmlWebpackPluginConfig],
+};
