8c543abdb0cb44d26f72d5cd391eda5c4c295450
randy-r <31799713+randy-r@users.noreply.github.com>
2017-09-10 11:59:37 +0300

    Inner lines for the outer/initial triangle.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 
 
+const InnerLines = ({ triangle }) => {
+  const { A, B, C } = triangle;
+  const side = B.x - A.x;
+  const L = { x: (B.x - A.x) / 2, y: A.y };
+  const M = { x: ((B.x - C.x) / 2) + (side / 2), y: (B.y - C.y) / 2 };
+  const N = { x: (C.x - A.x) / 2, y: (A.y - C.y) / 2 };
+
+  const pathData = `M${L.x} ${L.y} L${M.x} ${M.y} L${N.x} ${N.y} Z`;
+  return (
+    <path d={pathData} fill="none" stroke="blue" />
+  );
+};
+
 const InitialTriangle = () => {
   const side = 400;
-  const pickY = side - (Math.cos(Math.PI / 6) * side);
+  const height = (Math.cos(Math.PI / 6) * side);
+  const initialTriangle = {
+    A: { x: 0, y: height },
+    B: { x: side, y: height },
+    C: { x: side / 2, y: 0 },
+  };
+  const { A, B, C } = initialTriangle;
+  const pathData = `M${A.x} ${A.y} L${B.x} ${B.y} L${C.x} ${C.y} Z`;
   return (
-    <svg width={side} height={side} viewBox={`0 0 ${side} ${side}`} xmlns="http://www.w3.org/2000/svg">
-      <path
-        d={`M0 ${side} L${side} ${side} L${side / 2} ${pickY} Z`}
-        fill="none"
-        stroke="red"
-      />
-
+    <svg width={side} height={height} viewBox={`0 0 ${side + 10} ${height}`} xmlns="http://www.w3.org/2000/svg">
+      <path d={pathData} fill="none" stroke="red" />
+      <InnerLines triangle={initialTriangle} />
     </svg>
   );
 };
