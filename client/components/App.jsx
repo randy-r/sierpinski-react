b5f22ff186b2bb2416f474433b8448eddb807a66
randy-r <31799713+randy-r@users.noreply.github.com>
2017-09-10 19:51:13 +0300

    Generate triangle onClick.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { interpolateInferno } from 'd3-scale';
 
-const RECCURSION_LIMIT = 6;
+const MAX_RECURSION_LIMIT = 6;
 const totalTriangles =
-  [...Array(RECCURSION_LIMIT).keys()]
+  [...Array(MAX_RECURSION_LIMIT).keys()]
     .map(i => 3 ** i)
-    .reduce((a, b) => a + b); // (3 ** (RECCURSION_LIMIT));
+    .reduce((a, b) => a + b); // (3 ** (MAX_RECURSION_LIMIT));
 const colorDenominator = totalTriangles;
 let crtTotalTriangles = 0;
 
-const InnerLines = ({ triangle, crtReccDepth }) => {
-  if (crtReccDepth >= RECCURSION_LIMIT) {
+const InnerLines = ({ triangle, crtRecDepth, recursionLimit }) => {
+  if (crtRecDepth >= MAX_RECURSION_LIMIT || crtRecDepth >= recursionLimit) {
     return null;
   }
 
@@ -27,35 +27,61 @@ const InnerLines = ({ triangle, crtReccDepth }) => {
   const triangleB = { A: L, B, C: M };
   const triangleC = { A: N, B: M, C };
 
-  const newReccDepth = crtReccDepth + 1;
+  const newRecDepth = crtRecDepth + 1;
   const color = interpolateInferno(crtTotalTriangles / colorDenominator);
   return (
     <g >
       <path d={pathData} fill={color} stroke="none" />
-      <InnerLines triangle={triangleA} crtReccDepth={newReccDepth} />
-      <InnerLines triangle={triangleB} crtReccDepth={newReccDepth} />
-      <InnerLines triangle={triangleC} crtReccDepth={newReccDepth} />
+      <InnerLines triangle={triangleA} crtRecDepth={newRecDepth} recursionLimit={recursionLimit} />
+      <InnerLines triangle={triangleB} crtRecDepth={newRecDepth} recursionLimit={recursionLimit} />
+      <InnerLines triangle={triangleC} crtRecDepth={newRecDepth} recursionLimit={recursionLimit} />
     </g>
   );
 };
 
-const InitialTriangle = () => {
-  const side = 400;
-  const height = (Math.cos(Math.PI / 6) * side);
-  const initialTriangle = {
-    A: { x: 0, y: height },
-    B: { x: side, y: height },
-    C: { x: side / 2, y: 0 },
-  };
-  const { A, B, C } = initialTriangle;
-  const pathData = `M${A.x} ${A.y} L${B.x} ${B.y} L${C.x} ${C.y} Z`;
-  return (
-    <svg width={side} height={height} viewBox={`0 0 ${side + 10} ${height}`} xmlns="http://www.w3.org/2000/svg">
-      <path d={pathData} fill="none" stroke="none" />
-      <InnerLines triangle={initialTriangle} crtReccDepth={0} />
-    </svg>
-  );
-};
+class InitialTriangle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { recursionLimit: 1 };
+  }
+
+  handleMouseMove = (event) => {
+    const bcr = this.svg.getBoundingClientRect();
+    const x = event.pageX - bcr.left;
+    const y = event.pageY - bcr.top;
+    crtTotalTriangles = 0;
+    this.setState({ recursionLimit: this.state.recursionLimit + 1 });
+  }
+
+  render() {
+    const side = 400;
+    const height = (Math.cos(Math.PI / 6) * side);
+    const initialTriangle = {
+      A: { x: 0, y: height },
+      B: { x: side, y: height },
+      C: { x: side / 2, y: 0 },
+    };
+    const { A, B, C } = initialTriangle;
+    const pathData = `M${A.x} ${A.y} L${B.x} ${B.y} L${C.x} ${C.y} Z`;
+    return (
+      <svg
+        ref={(el) => { this.svg = el; }}
+        onClick={this.handleMouseMove}
+        width={side}
+        height={height}
+        viewBox={`0 0 ${side + 10} ${height}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d={pathData} fill="none" stroke="none" />
+        <InnerLines
+          triangle={initialTriangle}
+          crtRecDepth={0}
+          recursionLimit={this.state.recursionLimit}
+        />
+      </svg>
+    );
+  }
+}
 
 const App = () => <InitialTriangle />;
 
