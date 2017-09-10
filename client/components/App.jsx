8bf0ed8558400800c7d106f20eec8af141067110
randy-r <31799713+randy-r@users.noreply.github.com>
2017-09-10 11:15:03 +0300

    Outer triangle.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
-export default function App() {
+
+const InitialTriangle = () => {
+  const side = 400;
+  const pickY = side - (Math.cos(Math.PI / 6) * side);
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Hello World</h1>
-    </div>);
-}
+    <svg width={side} height={side} viewBox={`0 0 ${side} ${side}`} xmlns="http://www.w3.org/2000/svg">
+      <path
+        d={`M0 ${side} L${side} ${side} L${side / 2} ${pickY} Z`}
+        fill="none"
+        stroke="red"
+      />
+
+    </svg>
+  );
+};
+
+const App = () => <InitialTriangle />;
+
+
+export default App;
