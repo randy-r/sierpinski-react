c39b1fec2196144f435ed1754f6bc2c07aac7359
randy-r <31799713+randy-r@users.noreply.github.com>
2017-09-10 13:17:17 +0300

    First Sierpinski triangle rendered.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
+const RECCURSION_LIMIT = 4;
 
-const InnerLines = ({ triangle }) => {
+const InnerLines = ({ triangle, crtReccDepth }) => {
+  if (crtReccDepth >= RECCURSION_LIMIT) {
+    return null;
+  }
+
   const { A, B, C } = triangle;
   const side = B.x - A.x;
-  const L = { x: (B.x - A.x) / 2, y: A.y };
-  const M = { x: ((B.x - C.x) / 2) + (side / 2), y: (B.y - C.y) / 2 };
-  const N = { x: (C.x - A.x) / 2, y: (A.y - C.y) / 2 };
+  const L = { x: ((B.x - A.x) / 2) + A.x, y: A.y };
+  const M = { x: ((B.x - C.x) / 2) + (side / 2) + A.x, y: ((B.y - C.y) / 2) + C.y };
+  const N = { x: ((C.x - A.x) / 2) + A.x, y: ((A.y - C.y) / 2) + C.y };
 
   const pathData = `M${L.x} ${L.y} L${M.x} ${M.y} L${N.x} ${N.y} Z`;
+  const triangleA = { A, B: L, C: N };
+  const triangleB = { A: L, B, C: M };
+  const triangleC = { A: N, B: M, C };
   return (
-    <path d={pathData} fill="none" stroke="blue" />
+    <g >
+      <path d={pathData} fill="none" stroke="blue" />
+      <InnerLines triangle={triangleA} crtReccDepth={crtReccDepth + 1} />
+      <InnerLines triangle={triangleB} crtReccDepth={crtReccDepth + 1} />
+      <InnerLines triangle={triangleC} crtReccDepth={crtReccDepth + 1} />
+    </g>
   );
 };
 
@@ -27,7 +40,7 @@ const InitialTriangle = () => {
   return (
     <svg width={side} height={height} viewBox={`0 0 ${side + 10} ${height}`} xmlns="http://www.w3.org/2000/svg">
       <path d={pathData} fill="none" stroke="red" />
-      <InnerLines triangle={initialTriangle} />
+      <InnerLines triangle={initialTriangle} crtReccDepth={0} />
     </svg>
   );
 };
